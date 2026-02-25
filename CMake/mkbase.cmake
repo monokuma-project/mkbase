cf0c04523df099eb7116e56310217d633ca0d3a1
username000101 <cxx.username000101@gmail.com>
2026-02-25 17:11:10 +0300

    Add CMake files

---
+++ CMake/mkbase.cmake
@@ -0,0 +1,28 @@
+option(MKBASE_ERROR_AUTOTELL "Automatic log error when its constructed" OFF)
+
+if (BUILD_SHARED_LIBS)
+    add_library(mkbase SHARED)
+    target_compile_definitions(mkbase PRIVATE MKBASE_DLL)
+else ()
+    add_library(mkbase STATIC)
+endif ()
+
+target_include_directories(mkbase
+        PUBLIC "${CMAKE_SOURCE_DIR}/src"
+)
+
+if (MKBASE_ERROR_AUTOTELL)
+    target_compile_definitions(mkbase PRIVATE ERROR_AUTO_TELLING)
+endif ()
+
+target_compile_definitions(mkbase PRIVATE MKBASE_COMPILATION)
+
+file(GLOB MKBASE_SOURCES
+        "${CMAKE_SOURCE_DIR}/src/mkbase/byteio/*.cpp"
+        "${CMAKE_SOURCE_DIR}/src/mkbase/byteio/bytesarray/*.cpp"
+        "${CMAKE_SOURCE_DIR}/src/mkbase/byteio/bytestream/*.cpp"
+        "${CMAKE_SOURCE_DIR}/src/mkbase/byteio/filestream/*.cpp"
+        "${CMAKE_SOURCE_DIR}/src/mkbase/logging/*.cpp"
+)
+
+target_sources(mkbase PRIVATE ${MKBASE_SOURCES})
