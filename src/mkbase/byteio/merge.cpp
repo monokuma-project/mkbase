b6cfdd833c7479a2245aad62bd5387371d28c05b
username000101 <cxx.username000101@gmail.com>
2026-02-26 02:40:34 +0300

    Flip order of bytes and simplify implementation

--- src/mkbase/byteio/merge.cpp
+++ src/mkbase/byteio/merge.cpp
@@ -3,27 +3,20 @@
 #include <bit>
 
 monokuma::byteio::i32 monokuma::byteio::merge_bytes_to_i32(const std::array<byte, 4> &bytes) {
-     const auto result = static_cast<i32>(bytes[0]) << 24
-    | static_cast<i32>(bytes[1]) << 16
-    | static_cast<i32>(bytes[2]) << 8
-    | static_cast<i32>(bytes[3]);
-
-    if constexpr (std::endian::native == std::endian::little)
-        return std::byteswap(result);
-    else return result;
+    return static_cast<i32>(bytes[3]) << 24
+    | static_cast<i32>(bytes[2]) << 16
+    | static_cast<i32>(bytes[1]) << 8
+    | static_cast<i32>(bytes[0]);
 }
 
 monokuma::byteio::i64 monokuma::byteio::merge_bytes_to_i64(const std::array<byte, 8> &bytes) {
-    const auto result = static_cast<i64>(bytes[0]) << 56
-    | static_cast<i64>(bytes[1]) << 48
-    | static_cast<i64>(bytes[2]) << 40
-    | static_cast<i64>(bytes[3]) << 32
-    | static_cast<i64>(bytes[4]) << 24
-    | static_cast<i64>(bytes[5]) << 16
-    | static_cast<i64>(bytes[6]) << 8
-    | static_cast<i64>(bytes[7]);
+    return static_cast<i64>(bytes[7]) << 56
+    | static_cast<i64>(bytes[6]) << 48
+    | static_cast<i64>(bytes[5]) << 40
+    | static_cast<i64>(bytes[4]) << 32
+    | static_cast<i64>(bytes[3]) << 24
+    | static_cast<i64>(bytes[2]) << 16
+    | static_cast<i64>(bytes[1]) << 8
+    | static_cast<i64>(bytes[0]);
 
-    if constexpr (std::endian::native == std::endian::little)
-        return std::byteswap(result);
-    else return result;
 }
