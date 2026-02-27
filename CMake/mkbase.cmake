413a053a4783059531d8736b4a01898c3e049e2d
username000101 <cxx.username000101@gmail.com>
2026-02-28 02:16:57 +0300

    Change the library type to INTERFACE

--- CMake/mkbase.cmake
+++ CMake/mkbase.cmake
@@ -1,28 +1,19 @@
 option(MKBASE_ERROR_AUTOTELL "Automatic log error when its constructed" OFF)
 
+# Now library has only headers
 if (BUILD_SHARED_LIBS)
-    add_library(mkbase SHARED)
+    add_library(mkbase INTERFACE)
     target_compile_definitions(mkbase PRIVATE MKBASE_DLL)
 else ()
-    add_library(mkbase STATIC)
+    add_library(mkbase INTERFACE)
 endif ()
 
 target_include_directories(mkbase
-        PUBLIC "${CMAKE_CURRENT_LIST_DIR}/../src"
+        INTERFACE "${CMAKE_CURRENT_LIST_DIR}/../src"
 )
 
 if (MKBASE_ERROR_AUTOTELL)
-    target_compile_definitions(mkbase PUBLIC ERROR_AUTO_TELLING)
+    target_compile_definitions(mkbase INTERFACE ERROR_AUTO_TELLING)
 endif ()
 
-target_compile_definitions(mkbase PRIVATE MKBASE_COMPILATION)
-
-file(GLOB MKBASE_SOURCES
-        "${CMAKE_CURRENT_LIST_DIR}/../src/mkbase/byteio/*.cpp"
-        "${CMAKE_CURRENT_LIST_DIR}/../src/mkbase/byteio/bytesarray/*.cpp"
-        "${CMAKE_CURRENT_LIST_DIR}/../src/mkbase/byteio/bytestream/*.cpp"
-        "${CMAKE_CURRENT_LIST_DIR}/../src/mkbase/byteio/filestream/*.cpp"
-        "${CMAKE_CURRENT_LIST_DIR}/../src/mkbase/logging/*.cpp"
-)
-
-target_sources(mkbase PRIVATE ${MKBASE_SOURCES})
+target_compile_definitions(mkbase INTERFACE MKBASE_COMPILATION)
