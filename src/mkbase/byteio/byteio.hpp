09184e25be7cd603d7c706e3f63a7e14dafc1b43
username000101 <cxx.username000101@gmail.com>
2026-02-27 22:02:35 +0300

    Remove unused includes

--- src/mkbase/byteio/byteio.hpp
+++ src/mkbase/byteio/byteio.hpp
@@ -4,7 +4,5 @@
 
 #include "mkbase/api.hpp"
 #include "bytestream/bytestream.hpp"
-#include "bytesarray/bytesarray.hpp"
-#include "filestream/filestream.hpp"
 #include "int_operations.hpp"
 #include "types.hpp"
